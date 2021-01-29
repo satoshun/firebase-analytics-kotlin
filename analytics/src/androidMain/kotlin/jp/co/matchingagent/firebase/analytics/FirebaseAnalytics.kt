a4d623f553346b440e36b5d5e275c8aad8d2dbdc
SatoShun <shun.sato1@gmail.com>
2021-01-29 05:36:19 +0000

    move top level Param and Event to inner static

--- analytics/src/androidMain/kotlin/jp/co/matchingagent/firebase/analytics/FirebaseAnalytics.kt
+++ analytics/src/androidMain/kotlin/jp/co/matchingagent/firebase/analytics/FirebaseAnalytics.kt
@@ -72,6 +72,122 @@ actual class FirebaseAnalytics(
   fun setConsent(consentSettings: Map<ConsentType, ConsentStatus>) {
     analytics.setConsent(consentSettings)
   }
+
+  actual object Event {
+    actual val AD_IMPRESSION: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.AD_IMPRESSION
+    actual val ADD_PAYMENT_INFO: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.ADD_PAYMENT_INFO
+    actual val ADD_TO_CART: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.ADD_TO_CART
+    actual val ADD_TO_WISHLIST: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.ADD_TO_WISHLIST
+    actual val APP_OPEN: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.APP_OPEN
+    actual val BEGIN_CHECKOUT: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.BEGIN_CHECKOUT
+    actual val CAMPAIGN_DETAILS: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.CAMPAIGN_DETAILS
+    actual val ECOMMERCE_PURCHASE: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.ECOMMERCE_PURCHASE
+    actual val GENERATE_LEAD: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.GENERATE_LEAD
+    actual val JOIN_GROUP: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.JOIN_GROUP
+    actual val LEVEL_END: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.LEVEL_END
+    actual val LEVEL_START: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.LEVEL_START
+    actual val LEVEL_UP: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.LEVEL_UP
+    actual val LOGIN: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.LOGIN
+    actual val POST_SCORE: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.POST_SCORE
+    actual val PRESENT_OFFER: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.PRESENT_OFFER
+    actual val PURCHASE_REFUND: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.PURCHASE_REFUND
+    actual val SEARCH: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.SEARCH
+    actual val SELECT_CONTENT: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.SELECT_CONTENT
+    actual val SHARE: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.SHARE
+    actual val SIGN_UP: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.SIGN_UP
+    actual val SPEND_VIRTUAL_CURRENCY: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.SPEND_VIRTUAL_CURRENCY
+    actual val TUTORIAL_BEGIN: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.TUTORIAL_BEGIN
+    actual val TUTORIAL_COMPLETE: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.TUTORIAL_COMPLETE
+    actual val UNLOCK_ACHIEVEMENT: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.UNLOCK_ACHIEVEMENT
+    actual val VIEW_ITEM: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.VIEW_ITEM
+    actual val VIEW_ITEM_LIST: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.VIEW_ITEM_LIST
+    actual val VIEW_SEARCH_RESULTS: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.VIEW_SEARCH_RESULTS
+    actual val EARN_VIRTUAL_CURRENCY: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.EARN_VIRTUAL_CURRENCY
+    actual val SCREEN_VIEW: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.SCREEN_VIEW
+    actual val REMOVE_FROM_CART: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.REMOVE_FROM_CART
+    actual val CHECKOUT_PROGRESS: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.CHECKOUT_PROGRESS
+    actual val SET_CHECKOUT_OPTION: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.SET_CHECKOUT_OPTION
+    actual val ADD_SHIPPING_INFO: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.ADD_SHIPPING_INFO
+    actual val PURCHASE: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.PURCHASE
+    actual val REFUND: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.REFUND
+    actual val SELECT_ITEM: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.SELECT_ITEM
+    actual val SELECT_PROMOTION: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.SELECT_PROMOTION
+    actual val VIEW_CART: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.VIEW_CART
+    actual val VIEW_PROMOTION: String get() = com.google.firebase.analytics.FirebaseAnalytics.Event.VIEW_PROMOTION
+  }
+
+  actual object Param {
+    actual val ACHIEVEMENT_ID: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.ACHIEVEMENT_ID
+    actual val AD_FORMAT: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.AD_FORMAT
+    actual val AD_PLATFORM: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.AD_PLATFORM
+    actual val AD_SOURCE: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.AD_SOURCE
+    actual val AD_UNIT_NAME: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.AD_UNIT_NAME
+    actual val CHARACTER: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.CHARACTER
+    actual val TRAVEL_CLASS: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.TRAVEL_CLASS
+    actual val CONTENT_TYPE: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.CONTENT_TYPE
+    actual val CURRENCY: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.CURRENCY
+    actual val COUPON: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.COUPON
+    actual val START_DATE: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.START_DATE
+    actual val END_DATE: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.END_DATE
+    actual val EXTEND_SESSION: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.EXTEND_SESSION
+    actual val FLIGHT_NUMBER: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.FLIGHT_NUMBER
+    actual val GROUP_ID: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.GROUP_ID
+    actual val ITEM_CATEGORY: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.ITEM_CATEGORY
+    actual val ITEM_ID: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.ITEM_ID
+    actual val ITEM_LOCATION_ID: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.ITEM_LOCATION_ID
+    actual val ITEM_NAME: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.ITEM_NAME
+    actual val LOCATION: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.LOCATION
+    actual val LEVEL: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.LEVEL
+    actual val LEVEL_NAME: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.LEVEL_NAME
+    actual val SIGN_UP_METHOD: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.SIGN_UP_METHOD
+    actual val METHOD: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.METHOD
+    actual val NUMBER_OF_NIGHTS: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.NUMBER_OF_NIGHTS
+    actual val NUMBER_OF_PASSENGERS: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.NUMBER_OF_PASSENGERS
+    actual val NUMBER_OF_ROOMS: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.NUMBER_OF_ROOMS
+    actual val DESTINATION: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.DESTINATION
+    actual val ORIGIN: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.ORIGIN
+    actual val PRICE: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.PRICE
+    actual val QUANTITY: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.QUANTITY
+    actual val SCORE: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.SCORE
+    actual val SHIPPING: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.SHIPPING
+    actual val TRANSACTION_ID: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.TRANSACTION_ID
+    actual val SEARCH_TERM: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.SEARCH_TERM
+    actual val SUCCESS: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.SUCCESS
+    actual val TAX: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.TAX
+    actual val VALUE: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.VALUE
+    actual val VIRTUAL_CURRENCY_NAME: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.VIRTUAL_CURRENCY_NAME
+    actual val CAMPAIGN: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.CAMPAIGN
+    actual val SOURCE: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.SOURCE
+    actual val MEDIUM: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.MEDIUM
+    actual val TERM: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.TERM
+    actual val CONTENT: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.CONTENT
+    actual val ACLID: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.ACLID
+    actual val CP1: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.CP1
+    actual val ITEM_BRAND: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.ITEM_BRAND
+    actual val ITEM_VARIANT: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.ITEM_VARIANT
+    actual val ITEM_LIST: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.ITEM_LIST
+    actual val CHECKOUT_STEP: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.CHECKOUT_STEP
+    actual val CHECKOUT_OPTION: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.CHECKOUT_OPTION
+    actual val CREATIVE_NAME: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.CREATIVE_NAME
+    actual val CREATIVE_SLOT: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.CREATIVE_SLOT
+    actual val AFFILIATION: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.AFFILIATION
+    actual val INDEX: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.INDEX
+    actual val DISCOUNT: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.DISCOUNT
+    actual val ITEM_CATEGORY2: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.ITEM_CATEGORY2
+    actual val ITEM_CATEGORY3: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.ITEM_CATEGORY3
+    actual val ITEM_CATEGORY4: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.ITEM_CATEGORY4
+    actual val ITEM_CATEGORY5: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.ITEM_CATEGORY5
+    actual val ITEM_LIST_ID: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.ITEM_LIST_ID
+    actual val ITEM_LIST_NAME: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.ITEM_LIST_NAME
+    actual val ITEMS: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.ITEMS
+    actual val LOCATION_ID: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.LOCATION_ID
+    actual val PAYMENT_TYPE: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.PAYMENT_TYPE
+    actual val PROMOTION_ID: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.PROMOTION_ID
+    actual val PROMOTION_NAME: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.PROMOTION_NAME
+    actual val SCREEN_CLASS: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.SCREEN_CLASS
+    actual val SCREEN_NAME: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.SCREEN_NAME
+    actual val SHIPPING_TIER: String get() = com.google.firebase.analytics.FirebaseAnalytics.Param.SHIPPING_TIER
+  }
 }
 
 actual typealias FirebaseParametersBuilder = com.google.firebase.analytics.ktx.ParametersBuilder
--- analytics/src/iosMain/kotlin/jp/co/matchingagent/firebase/analytics/FirebaseAnalytics.kt
+++ analytics/src/iosMain/kotlin/jp/co/matchingagent/firebase/analytics/FirebaseAnalytics.kt
@@ -1,6 +1,114 @@
 package jp.co.matchingagent.firebase.analytics
 
 import cocoapods.FirebaseAnalytics.FIRAnalyticsMeta
+import cocoapods.FirebaseAnalytics.kFIREventAdImpression
+import cocoapods.FirebaseAnalytics.kFIREventAddPaymentInfo
+import cocoapods.FirebaseAnalytics.kFIREventAddShippingInfo
+import cocoapods.FirebaseAnalytics.kFIREventAddToCart
+import cocoapods.FirebaseAnalytics.kFIREventAddToWishlist
+import cocoapods.FirebaseAnalytics.kFIREventAppOpen
+import cocoapods.FirebaseAnalytics.kFIREventBeginCheckout
+import cocoapods.FirebaseAnalytics.kFIREventCampaignDetails
+import cocoapods.FirebaseAnalytics.kFIREventCheckoutProgress
+import cocoapods.FirebaseAnalytics.kFIREventEarnVirtualCurrency
+import cocoapods.FirebaseAnalytics.kFIREventEcommercePurchase
+import cocoapods.FirebaseAnalytics.kFIREventGenerateLead
+import cocoapods.FirebaseAnalytics.kFIREventJoinGroup
+import cocoapods.FirebaseAnalytics.kFIREventLevelEnd
+import cocoapods.FirebaseAnalytics.kFIREventLevelStart
+import cocoapods.FirebaseAnalytics.kFIREventLevelUp
+import cocoapods.FirebaseAnalytics.kFIREventLogin
+import cocoapods.FirebaseAnalytics.kFIREventPostScore
+import cocoapods.FirebaseAnalytics.kFIREventPresentOffer
+import cocoapods.FirebaseAnalytics.kFIREventPurchase
+import cocoapods.FirebaseAnalytics.kFIREventPurchaseRefund
+import cocoapods.FirebaseAnalytics.kFIREventRefund
+import cocoapods.FirebaseAnalytics.kFIREventRemoveFromCart
+import cocoapods.FirebaseAnalytics.kFIREventScreenView
+import cocoapods.FirebaseAnalytics.kFIREventSearch
+import cocoapods.FirebaseAnalytics.kFIREventSelectContent
+import cocoapods.FirebaseAnalytics.kFIREventSelectItem
+import cocoapods.FirebaseAnalytics.kFIREventSelectPromotion
+import cocoapods.FirebaseAnalytics.kFIREventSetCheckoutOption
+import cocoapods.FirebaseAnalytics.kFIREventShare
+import cocoapods.FirebaseAnalytics.kFIREventSignUp
+import cocoapods.FirebaseAnalytics.kFIREventSpendVirtualCurrency
+import cocoapods.FirebaseAnalytics.kFIREventTutorialBegin
+import cocoapods.FirebaseAnalytics.kFIREventTutorialComplete
+import cocoapods.FirebaseAnalytics.kFIREventUnlockAchievement
+import cocoapods.FirebaseAnalytics.kFIREventViewCart
+import cocoapods.FirebaseAnalytics.kFIREventViewItem
+import cocoapods.FirebaseAnalytics.kFIREventViewItemList
+import cocoapods.FirebaseAnalytics.kFIREventViewPromotion
+import cocoapods.FirebaseAnalytics.kFIREventViewSearchResults
+import cocoapods.FirebaseAnalytics.kFIRParameterAchievementID
+import cocoapods.FirebaseAnalytics.kFIRParameterAdFormat
+import cocoapods.FirebaseAnalytics.kFIRParameterAdPlatform
+import cocoapods.FirebaseAnalytics.kFIRParameterAdSource
+import cocoapods.FirebaseAnalytics.kFIRParameterAdUnitName
+import cocoapods.FirebaseAnalytics.kFIRParameterAffiliation
+import cocoapods.FirebaseAnalytics.kFIRParameterCP1
+import cocoapods.FirebaseAnalytics.kFIRParameterCampaign
+import cocoapods.FirebaseAnalytics.kFIRParameterCharacter
+import cocoapods.FirebaseAnalytics.kFIRParameterCheckoutOption
+import cocoapods.FirebaseAnalytics.kFIRParameterCheckoutStep
+import cocoapods.FirebaseAnalytics.kFIRParameterContent
+import cocoapods.FirebaseAnalytics.kFIRParameterContentType
+import cocoapods.FirebaseAnalytics.kFIRParameterCoupon
+import cocoapods.FirebaseAnalytics.kFIRParameterCreativeName
+import cocoapods.FirebaseAnalytics.kFIRParameterCreativeSlot
+import cocoapods.FirebaseAnalytics.kFIRParameterCurrency
+import cocoapods.FirebaseAnalytics.kFIRParameterDestination
+import cocoapods.FirebaseAnalytics.kFIRParameterDiscount
+import cocoapods.FirebaseAnalytics.kFIRParameterEndDate
+import cocoapods.FirebaseAnalytics.kFIRParameterExtendSession
+import cocoapods.FirebaseAnalytics.kFIRParameterFlightNumber
+import cocoapods.FirebaseAnalytics.kFIRParameterGroupID
+import cocoapods.FirebaseAnalytics.kFIRParameterIndex
+import cocoapods.FirebaseAnalytics.kFIRParameterItemBrand
+import cocoapods.FirebaseAnalytics.kFIRParameterItemCategory
+import cocoapods.FirebaseAnalytics.kFIRParameterItemCategory2
+import cocoapods.FirebaseAnalytics.kFIRParameterItemCategory3
+import cocoapods.FirebaseAnalytics.kFIRParameterItemCategory4
+import cocoapods.FirebaseAnalytics.kFIRParameterItemCategory5
+import cocoapods.FirebaseAnalytics.kFIRParameterItemID
+import cocoapods.FirebaseAnalytics.kFIRParameterItemList
+import cocoapods.FirebaseAnalytics.kFIRParameterItemListID
+import cocoapods.FirebaseAnalytics.kFIRParameterItemListName
+import cocoapods.FirebaseAnalytics.kFIRParameterItemName
+import cocoapods.FirebaseAnalytics.kFIRParameterItemVariant
+import cocoapods.FirebaseAnalytics.kFIRParameterItems
+import cocoapods.FirebaseAnalytics.kFIRParameterLevel
+import cocoapods.FirebaseAnalytics.kFIRParameterLevelName
+import cocoapods.FirebaseAnalytics.kFIRParameterLocation
+import cocoapods.FirebaseAnalytics.kFIRParameterLocationID
+import cocoapods.FirebaseAnalytics.kFIRParameterMedium
+import cocoapods.FirebaseAnalytics.kFIRParameterMethod
+import cocoapods.FirebaseAnalytics.kFIRParameterNumberOfNights
+import cocoapods.FirebaseAnalytics.kFIRParameterNumberOfPassengers
+import cocoapods.FirebaseAnalytics.kFIRParameterNumberOfRooms
+import cocoapods.FirebaseAnalytics.kFIRParameterOrigin
+import cocoapods.FirebaseAnalytics.kFIRParameterPaymentType
+import cocoapods.FirebaseAnalytics.kFIRParameterPrice
+import cocoapods.FirebaseAnalytics.kFIRParameterPromotionID
+import cocoapods.FirebaseAnalytics.kFIRParameterPromotionName
+import cocoapods.FirebaseAnalytics.kFIRParameterQuantity
+import cocoapods.FirebaseAnalytics.kFIRParameterScore
+import cocoapods.FirebaseAnalytics.kFIRParameterScreenClass
+import cocoapods.FirebaseAnalytics.kFIRParameterScreenName
+import cocoapods.FirebaseAnalytics.kFIRParameterSearchTerm
+import cocoapods.FirebaseAnalytics.kFIRParameterShipping
+import cocoapods.FirebaseAnalytics.kFIRParameterShippingTier
+import cocoapods.FirebaseAnalytics.kFIRParameterSignUpMethod
+import cocoapods.FirebaseAnalytics.kFIRParameterSource
+import cocoapods.FirebaseAnalytics.kFIRParameterStartDate
+import cocoapods.FirebaseAnalytics.kFIRParameterSuccess
+import cocoapods.FirebaseAnalytics.kFIRParameterTax
+import cocoapods.FirebaseAnalytics.kFIRParameterTerm
+import cocoapods.FirebaseAnalytics.kFIRParameterTransactionID
+import cocoapods.FirebaseAnalytics.kFIRParameterTravelClass
+import cocoapods.FirebaseAnalytics.kFIRParameterValue
+import cocoapods.FirebaseAnalytics.kFIRParameterVirtualCurrencyName
 import jp.co.matchingagent.firebase.Firebase
 import cocoapods.FirebaseAnalytics.FIRAnalytics as fIRAnalytics
 
@@ -57,12 +165,128 @@ actual class FirebaseAnalytics(
     analytics.setUserPropertyString(name, value)
   }
 
-  fun setScreenName(screenName :String?, screenClass: String?) {
+  fun setScreenName(screenName: String?, screenClass: String?) {
     analytics.setScreenName(screenName, screenClass)
   }
 
   fun setDefaultEventParameters(parameters: Map<String, *>?) {
     analytics.setDefaultEventParameters(parameters?.toMap())
+  }
+
+  actual object Event {
+    actual val AD_IMPRESSION: String get() = kFIREventAdImpression!!
+    actual val ADD_PAYMENT_INFO: String get() = kFIREventAddPaymentInfo!!
+    actual val ADD_TO_CART: String get() = kFIREventAddToCart!!
+    actual val ADD_TO_WISHLIST: String get() = kFIREventAddToWishlist!!
+    actual val APP_OPEN: String get() = kFIREventAppOpen!!
+    actual val BEGIN_CHECKOUT: String get() = kFIREventBeginCheckout!!
+    actual val CAMPAIGN_DETAILS: String get() = kFIREventCampaignDetails!!
+    actual val ECOMMERCE_PURCHASE: String get() = kFIREventEcommercePurchase!!
+    actual val GENERATE_LEAD: String get() = kFIREventGenerateLead!!
+    actual val JOIN_GROUP: String get() = kFIREventJoinGroup!!
+    actual val LEVEL_END: String get() = kFIREventLevelEnd!!
+    actual val LEVEL_START: String get() = kFIREventLevelStart!!
+    actual val LEVEL_UP: String get() = kFIREventLevelUp!!
+    actual val LOGIN: String get() = kFIREventLogin!!
+    actual val POST_SCORE: String get() = kFIREventPostScore!!
+    actual val PRESENT_OFFER: String get() = kFIREventPresentOffer!!
+    actual val PURCHASE_REFUND: String get() = kFIREventPurchaseRefund!!
+    actual val SEARCH: String get() = kFIREventSearch!!
+    actual val SELECT_CONTENT: String get() = kFIREventSelectContent!!
+    actual val SHARE: String get() = kFIREventShare!!
+    actual val SIGN_UP: String get() = kFIREventSignUp!!
+    actual val SPEND_VIRTUAL_CURRENCY: String get() = kFIREventSpendVirtualCurrency!!
+    actual val TUTORIAL_BEGIN: String get() = kFIREventTutorialBegin!!
+    actual val TUTORIAL_COMPLETE: String get() = kFIREventTutorialComplete!!
+    actual val UNLOCK_ACHIEVEMENT: String get() = kFIREventUnlockAchievement!!
+    actual val VIEW_ITEM: String get() = kFIREventViewItem!!
+    actual val VIEW_ITEM_LIST: String get() = kFIREventViewItemList!!
+    actual val VIEW_SEARCH_RESULTS: String get() = kFIREventViewSearchResults!!
+    actual val EARN_VIRTUAL_CURRENCY: String get() = kFIREventEarnVirtualCurrency!!
+    actual val SCREEN_VIEW: String get() = kFIREventScreenView!!
+    actual val REMOVE_FROM_CART: String get() = kFIREventRemoveFromCart!!
+    actual val CHECKOUT_PROGRESS: String get() = kFIREventCheckoutProgress!!
+    actual val SET_CHECKOUT_OPTION: String get() = kFIREventSetCheckoutOption!!
+    actual val ADD_SHIPPING_INFO: String get() = kFIREventAddShippingInfo!!
+    actual val PURCHASE: String get() = kFIREventPurchase!!
+    actual val REFUND: String get() = kFIREventRefund!!
+    actual val SELECT_ITEM: String get() = kFIREventSelectItem!!
+    actual val SELECT_PROMOTION: String get() = kFIREventSelectPromotion!!
+    actual val VIEW_CART: String get() = kFIREventViewCart!!
+    actual val VIEW_PROMOTION: String get() = kFIREventViewPromotion!!
+  }
+
+  actual object Param {
+    actual val ACHIEVEMENT_ID: String get() = kFIRParameterAchievementID!!
+    actual val AD_FORMAT: String get() = kFIRParameterAdFormat!!
+    actual val AD_PLATFORM: String get() = kFIRParameterAdPlatform!!
+    actual val AD_SOURCE: String get() = kFIRParameterAdSource!!
+    actual val AD_UNIT_NAME: String get() = kFIRParameterAdUnitName!!
+    actual val CHARACTER: String get() = kFIRParameterCharacter!!
+    actual val TRAVEL_CLASS: String get() = kFIRParameterTravelClass!!
+    actual val CONTENT_TYPE: String get() = kFIRParameterContentType!!
+    actual val CURRENCY: String get() = kFIRParameterCurrency!!
+    actual val COUPON: String get() = kFIRParameterCoupon!!
+    actual val START_DATE: String get() = kFIRParameterStartDate!!
+    actual val END_DATE: String get() = kFIRParameterEndDate!!
+    actual val EXTEND_SESSION: String get() = kFIRParameterExtendSession!!
+    actual val FLIGHT_NUMBER: String get() = kFIRParameterFlightNumber!!
+    actual val GROUP_ID: String get() = kFIRParameterGroupID!!
+    actual val ITEM_CATEGORY: String get() = kFIRParameterItemCategory!!
+    actual val ITEM_ID: String get() = kFIRParameterItemID!!
+    actual val ITEM_LOCATION_ID: String get() = kFIRParameterLocationID!!
+    actual val ITEM_NAME: String get() = kFIRParameterItemName!!
+    actual val LOCATION: String get() = kFIRParameterLocation!!
+    actual val LEVEL: String get() = kFIRParameterLevel!!
+    actual val LEVEL_NAME: String get() = kFIRParameterLevelName!!
+    actual val SIGN_UP_METHOD: String get() = kFIRParameterSignUpMethod!!
+    actual val METHOD: String get() = kFIRParameterMethod!!
+    actual val NUMBER_OF_NIGHTS: String get() = kFIRParameterNumberOfNights!!
+    actual val NUMBER_OF_PASSENGERS: String get() = kFIRParameterNumberOfPassengers!!
+    actual val NUMBER_OF_ROOMS: String get() = kFIRParameterNumberOfRooms!!
+    actual val DESTINATION: String get() = kFIRParameterDestination!!
+    actual val ORIGIN: String get() = kFIRParameterOrigin!!
+    actual val PRICE: String get() = kFIRParameterPrice!!
+    actual val QUANTITY: String get() = kFIRParameterQuantity!!
+    actual val SCORE: String get() = kFIRParameterScore!!
+    actual val SHIPPING: String get() = kFIRParameterShipping!!
+    actual val TRANSACTION_ID: String get() = kFIRParameterTransactionID!!
+    actual val SEARCH_TERM: String get() = kFIRParameterSearchTerm!!
+    actual val SUCCESS: String get() = kFIRParameterSuccess!!
+    actual val TAX: String get() = kFIRParameterTax!!
+    actual val VALUE: String get() = kFIRParameterValue!!
+    actual val VIRTUAL_CURRENCY_NAME: String get() = kFIRParameterVirtualCurrencyName!!
+    actual val CAMPAIGN: String get() = kFIRParameterCampaign!!
+    actual val SOURCE: String get() = kFIRParameterSource!!
+    actual val MEDIUM: String get() = kFIRParameterMedium!!
+    actual val TERM: String get() = kFIRParameterTerm!!
+    actual val CONTENT: String get() = kFIRParameterContent!!
+    actual val ACLID: String get() = "aclid"
+    actual val CP1: String get() = kFIRParameterCP1!!
+    actual val ITEM_BRAND: String get() = kFIRParameterItemBrand!!
+    actual val ITEM_VARIANT: String get() = kFIRParameterItemVariant!!
+    actual val ITEM_LIST: String get() = kFIRParameterItemList!!
+    actual val CHECKOUT_STEP: String get() = kFIRParameterCheckoutStep!!
+    actual val CHECKOUT_OPTION: String get() = kFIRParameterCheckoutOption!!
+    actual val CREATIVE_NAME: String get() = kFIRParameterCreativeName!!
+    actual val CREATIVE_SLOT: String get() = kFIRParameterCreativeSlot!!
+    actual val AFFILIATION: String get() = kFIRParameterAffiliation!!
+    actual val INDEX: String get() = kFIRParameterIndex!!
+    actual val DISCOUNT: String get() = kFIRParameterDiscount!!
+    actual val ITEM_CATEGORY2: String get() = kFIRParameterItemCategory2!!
+    actual val ITEM_CATEGORY3: String get() = kFIRParameterItemCategory3!!
+    actual val ITEM_CATEGORY4: String get() = kFIRParameterItemCategory4!!
+    actual val ITEM_CATEGORY5: String get() = kFIRParameterItemCategory5!!
+    actual val ITEM_LIST_ID: String get() = kFIRParameterItemListID!!
+    actual val ITEM_LIST_NAME: String get() = kFIRParameterItemListName!!
+    actual val ITEMS: String get() = kFIRParameterItems!!
+    actual val LOCATION_ID: String get() = kFIRParameterLocationID!!
+    actual val PAYMENT_TYPE: String get() = kFIRParameterPaymentType!!
+    actual val PROMOTION_ID: String get() = kFIRParameterPromotionID!!
+    actual val PROMOTION_NAME: String get() = kFIRParameterPromotionName!!
+    actual val SCREEN_CLASS: String get() = kFIRParameterScreenClass!!
+    actual val SCREEN_NAME: String get() = kFIRParameterScreenName!!
+    actual val SHIPPING_TIER: String get() = kFIRParameterShippingTier!!
   }
 }
 
