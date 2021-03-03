c167a6a299f9e7cf48c9c2b16aa1ba60832fb878
SatoShun <shun.sato1@gmail.com>
2021-03-03 01:36:14 +0000

    kotlin 1.4.31

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     mavenCentral()
   }
   dependencies {
-    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.30")
+    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.31")
     classpath("com.android.tools.build:gradle:4.1.2")
     classpath("com.google.gms:google-services:4.3.5")
     classpath("org.jetbrains.dokka:dokka-gradle-plugin:0.9.18")
