54ad958b0d21c42e15a3ce667b0a9c3d92ed9ca8
SatoShun <shun.sato1@gmail.com>
2021-06-07 07:13:46 +0000

    agp 4.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
   }
   dependencies {
     classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.31")
-    classpath("com.android.tools.build:gradle:4.1.2")
+    classpath("com.android.tools.build:gradle:4.2.1")
     classpath("com.google.gms:google-services:4.3.5")
     classpath("org.jetbrains.dokka:dokka-gradle-plugin:0.9.18")
     classpath("com.vanniktech:gradle-maven-publish-plugin:0.14.2")
