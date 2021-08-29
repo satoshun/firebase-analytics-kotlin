1a8dae3167ec5979b593533f24ade27073c22af7
SatoShun <shun.sato1@gmail.com>
2021-08-29 02:10:00 +0000

    add sampleShared module

---
+++ sampleShared/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+  kotlin("multiplatform")
+  id("com.android.library")
+}
+
+kotlin {
+  android()
+  ios {
+    binaries {
+      framework {
+        baseName = "shared"
+      }
+    }
+  }
+  sourceSets {
+    val commonMain by getting {
+      dependencies {
+        api(project(":analytics"))
+        //  implementation("com.github.satoshun.firebase:analytics:0.0.5")
+      }
+    }
+    val commonTest by getting {
+      dependencies {
+        implementation(kotlin("test-common"))
+        implementation(kotlin("test-annotations-common"))
+      }
+    }
+    val androidMain by getting {
+      dependencies {
+      }
+    }
+    val androidTest by getting {
+      dependencies {
+        implementation(kotlin("test-junit"))
+        implementation("junit:junit:4.13.2")
+      }
+    }
+    val iosMain by getting {
+      dependencies {
+      }
+    }
+    val iosTest by getting
+  }
+}
+
+android {
+  compileSdk = 30
+  sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+  defaultConfig {
+    minSdk = 21
+    targetSdk = 30
+  }
+}
