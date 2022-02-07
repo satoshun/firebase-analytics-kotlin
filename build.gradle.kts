82bc2591592171a952d769777c884397271e6b69
SatoShun <shun.sato1@gmail.com>
2022-02-07 06:00:00 +0000

    update agp

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     mavenCentral()
   }
   dependencies {
-    classpath("com.android.tools.build:gradle:7.1.0")
+    classpath("com.android.tools.build:gradle:7.1.1")
     classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
     classpath("com.google.gms:google-services:4.3.5")
     classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.4.32")
@@ -20,5 +20,5 @@ allprojects {
   }
 
   group = "com.github.satoshun.firebase"
-  version = "1.5.0"
+  version = "1.6.1"
 }
